0f0e1854e7fb6237959269fff814e303f1d514d7
EmilioMimi <45314344+EmilioMimi@users.noreply.github.com>
2018-11-24 17:05:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const cfg = require('index.json');
-const token = process.env.token 
+const token = process.env.token; 
 const prefix = (cfg.prefix);
 bot.on('ready', function () {
     console.log("Je suis prêt à être utilisé.")
